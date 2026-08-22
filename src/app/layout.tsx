b2d115d19d8eccb800560b735737884ff89cacba
Izuku <zklq1e@gmail.com>
2026-08-23 01:43:21 +0530

    Add god-view public map dashboard with CartoDB dark tiles, real PostGIS data, and glassmorphism sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,14 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
